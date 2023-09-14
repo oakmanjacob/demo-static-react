b73bfb7e5a3719e18bea6807d66f111f59d5afdd
Jacob Oakman <jacoboakman@gmail.com>
2023-09-13 22:35:50 -0700

    Updated function to return as expected

--- api/message/index.js
+++ api/message/index.js
@@ -1,6 +1,5 @@
 module.exports = async function (context, req) {
-    context.res.json = {
-        // status: 200, /* Defaults to 200 */
+    context.res.json({
         text: "Hello from the API"
-    };
-}
+    });
+};
